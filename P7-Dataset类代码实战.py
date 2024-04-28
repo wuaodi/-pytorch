c91041de13799df0ee864764039f68dc0ee77b75
wuaodi <2582469681@qq.com>
2024-04-28 23:17:45 +0800

    Dataset类代码实现

---
+++ P7-Dataset类代码实战.py
@@ -0,0 +1,31 @@
+from torch.utils.data import Dataset
+from PIL import Image
+import os
+
+
+class MyData(Dataset):
+    def __init__(self, root_dir, label_dir):
+        self.root = root_dir
+        self.label_fir = label_dir
+        self.image_dir = os.path.join(self.root, self.label_fir)
+        self.image_list = os.listdir(self.image_dir)
+
+    def __getitem__(self, idx):
+        """重写[]方法的调用"""
+        img_path = os.path.join(self.image_dir, self.image_list[idx])
+        img = Image.open(img_path)
+        label = self.label_fir
+        return img, label
+
+    def __len__(self):
+        """重写len()方法的调用"""
+        return len(self.image_list)
+
+
+ants_data = MyData("hymenoptera_data/train", "ants")
+bees_data = MyData("hymenoptera_data/train", "bees")
+# 两个小的数据集可以直接叠加，父类Dataset中有实现
+dataset = ants_data + bees_data
+image, label = dataset[125]
+image.show()
+print(len(dataset))
